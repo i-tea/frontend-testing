719bc3fd52d95b4383f627bc2d278837008c59ff
Petr Pavelka <petr.pavelka@i-projekt.cz>
2017-06-13 17:00:24 +0200

    slick basic configuration

--- dev/scripts/index.js
+++ dev/scripts/index.js
@@ -5,38 +5,35 @@ var $ = jQuery.noConflict();
 (function( $ ) {
     $(document).ready(function() {
         $('.feature-products-row').slick({
-        dots: true,
-        infinite: false,
+        dots: false,
+        infinite: true,
         speed: 300,
         slidesToShow: 4,
         slidesToScroll: 4,
         responsive: [
             {
-            breakpoint: 1024,
+            breakpoint: 1200,
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: true
+                dots: false
             }
             },
             {
-            breakpoint: 600,
+            breakpoint: 992,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
             }
             },
             {
-            breakpoint: 480,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
             }
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
         ]
         });
     });
